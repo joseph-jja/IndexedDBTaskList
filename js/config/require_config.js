301ed7eb05861ecb684ec21180f5efc5bec44ffe
Joseph Acosta <joseph.acosta@macys.com>
2014-11-18 13:05:15 -0800

    add in libs and require config and update template

---
+++ js/config/require_config.js
@@ -0,0 +1,37 @@
+(function() {
+
+    var paths = {
+        jquery: 'libs/jquery-1.11.1',
+        handlebars: 'libs/handlebars-v1.3.0',
+        backbone: 'libs/backbone',
+        underscore: 'libs/underscore',
+        taskList: '/js/views/TaskList',
+        taskModel: '/js/models/Task',
+        taskView: '/js/views/Task',
+        constants: '/js/Constants',
+        indexedDB: '/js/IndexedDB',
+        templateDir: '/templates'
+    };
+
+    var shims = {
+        jquery: {
+            exports: 'jQuery'
+        },
+        handlebars: {
+            exports: 'Handlebars'
+        },
+        backbone: {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        underscore: {
+            exports: '_'
+        }
+    };
+
+    require.config({
+        paths: paths,
+        shim: shims
+    });
+
+})();
